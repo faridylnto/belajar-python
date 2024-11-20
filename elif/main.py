2a62d8033d4a1dbede7fab4ec408b143ffb573ba
Muhammad Farid Yulianto <yuliantofarid11@gmail.com>
2024-11-20 21:46:23 +0700

    elif

---
+++ elif/main.py
@@ -0,0 +1,14 @@
+# ELIF else if statement
+
+nama = input("Nama Kamu Siapa? ")
+
+if nama == "ucup": # kondisi 1
+    print("Hai Ucup") # aksi true 1
+elif nama == "otong": # kondisi 2
+    print("Hai otong")
+elif nama == "mario":
+    print("Hai mario")
+else:
+    print("Maaf, saya tidak kenal")
+    
+print("ini adalah akhir program")
